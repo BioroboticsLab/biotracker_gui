e0d82811accdcafcf862183b28cc2bf22dc063e2
justayak <julian.tanke@googlemail.com>
2016-01-02 00:51:58 +0000

    simplify zoom functions and use of biotracker api

--- biotracker/src/VideoView.cpp
+++ biotracker/src/VideoView.cpp
@@ -172,24 +172,13 @@ void VideoView::mouseMoveEvent(QMouseEvent *e) {
 
     switch (m_currentMode) {
         case Mode::PANZOOM: {
-
-            QPoint stuff = Core::ScreenHelper::screenToImageCoords(
-                m_panZoomState,
-                m_texture.getImage().cols,
-                m_texture.getImage().rows,
-                width(),
-                height(),
-                QPoint(0,0));
-
             if (m_panZoomState.panState) {
                 const QPointF delta = e->localPos() - (*m_panZoomState.panState).lastPos;
                 (*m_panZoomState.panState).lastPos = e->localPos();
-
                 m_panZoomState.isChanged = true;
                 m_panZoomState.panX -= static_cast<float>(delta.x());
                 m_panZoomState.panY -= static_cast<float>(delta.y());
                 update();
-            } else {
             }
             break;
         }
@@ -260,50 +249,18 @@ void VideoView::mouseReleaseEvent(QMouseEvent *e) {
 }
 
 void VideoView::wheelEvent(QWheelEvent *e) {
-    // The maximum zoom is defined such that one image pixel can never become bigger than the widget size. The zoom
-    // step size is calculated based on the ratio of widget size and image size and decays exponentially when zooming
-    // in.
-    //float step = 0.0005f * m_screenPicRatio;
-    float step = 1;
-    if ((m_panZoomState.zoomFactor / m_screenPicRatio) < 0.f) {
-        //step *= 1.f + (m_panZoomState.zoomFactor / m_screenPicRatio);
-    }
-
     switch (m_currentMode) {
         case Mode::PANZOOM: {
             if (e->orientation() == Qt::Vertical) {
-                const QPointF pos = e->posF();
                 const int numDegrees  = e->delta();
                 const float deltaZoom = step * numDegrees;
-
-                const float oldZoomFactor = 1 + m_panZoomState.zoomFactor;
-
-                QPoint imPos = BioTracker::Core::ScreenHelper::screenToImageCoords(
+                m_panZoomState = BioTracker::Core::ScreenHelper::zoomTo(
                     m_panZoomState,
                     m_texture.getImage().cols, m_texture.getImage().rows,
                     this->width(), this->height(),
+                    deltaZoom,
                     e->pos()
                 );
-
-                m_panZoomState.zoomFactor -= deltaZoom/1200;
-                m_panZoomState.isChanged = true;
-
-                const float delta_zoom = oldZoomFactor - m_panZoomState.zoomFactor;
-
-
-                float propX = -30 * delta_zoom;
-                float propY = 0;
-
-                float zoom = 1 + m_panZoomState.zoomFactor;
-
-                // reset to non-zoomed coordinates
-                const float oldPanX = m_panZoomState.panX / oldZoomFactor;
-                const float oldPanY = m_panZoomState.panY / oldZoomFactor;
-
-                m_panZoomState.panX = (oldPanX - propX) * zoom;
-                m_panZoomState.panY = (oldPanY - propY) * zoom;
-
-                resizeGL(this->width(), this->height());
                 update();
                 e->accept();
             }
