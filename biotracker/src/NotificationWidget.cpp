49435372611768763cfc6b6492d7c0d273e2f9ac
justayak <julian.tanke@googlemail.com>
2016-04-05 10:43:23 +0000

    different colors are used for representing different types of messages in the message box

--- biotracker/src/NotificationWidget.cpp
+++ biotracker/src/NotificationWidget.cpp
@@ -17,16 +17,22 @@ void NotificationWidget::initConnects() {
 
 void NotificationWidget::notify(const std::string &message,
                                 const Core::MessageType type) {
+    std::stringstream ss;
     switch (type) {
     case Core::MessageType::FILE_OPEN:
-        m_ui.edit_notification->append(QString(message.c_str()));
+        ss << "<span style='color:#4c4cc9;'>" << message << "</span>";
         break;
     case Core::MessageType::NOTIFICATION:
-        m_ui.edit_notification->append(QString(message.c_str()));
+        ss << "<span>" << message << "</span>";
+        break;
+    case Core::MessageType::FAIL:
+        ss << "<span style='color:red; font-weight:bold;'>" << message << "</span>";
         break;
     default:
-        ;
+        assert(false);
     }
+    ss << "<br>";
+    m_ui.edit_notification->insertHtml(QString(ss.str().c_str()));
 
 }
 
